1d03da7a4f91644a09a16a39d3b9462749ba144f
Jaleed <k224912@nu.edu.pk>
2023-05-11 08:11:50 +0500

    back to original format of port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 const server=app.listen(PORT,console.log(`server Started on PORT  ${PORT}`));
 
 const io = require("socket.io")(server, {
