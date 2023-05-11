e5016dc758521cb0011041533dd751f7e647a140
Jaleed <k224912@nu.edu.pk>
2023-05-11 07:43:46 +0500

    removed the 5000

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
-const server=app.listen(PORT,console.log(`server Started on PORT 5000 ${PORT}`));
+const server=app.listen(PORT,console.log(`server Started on PORT  ${PORT}`));
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,//after this time it will be inactice
